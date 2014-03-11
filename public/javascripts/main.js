3dd82334fbbdc4bc4f7b3e4f8b1c840067e92c0e
arvindayal <arvinddayal@gmail.com>
2014-03-11 16:12:42 -0600

    minor changes

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -9,11 +9,17 @@ $(function(){
 			$('#travel').attr('disabled', 'disabled');
 			$('.travel').hide();
 		}
-		else if ($('#purpose').val()==="travel") {
+		else if ($('#purpose').val()==="vehicle") {
 			$('#travel').attr('disabled', false);
 			$('.travel').show();
 			$('.homeGroup').attr('disabled', 'disabled');
 			$('.home').hide();
+		}
+		else if ($('#purpose').val()==="purpose") {
+			$('.homeGroup').attr('disabled', false);
+			$('.home').show();
+			$('#travel').attr('disabled', false);
+			$('.travel').show();
 		}
 	});
 
