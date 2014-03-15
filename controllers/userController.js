89c26da9c1cc2c7a2cb801490921f5e9617d02e1
arvindayal <arvinddayal@gmail.com>
2014-03-14 20:37:52 -0600

    need to fix duplicates in kit db

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,15 +7,80 @@ module.exports = {
 		QuestionnaireModel.findOne({}).sort("-timeCreated").exec( function(err,doc){
 			var query = {"userid": req.user.userid};
 			var newKitName = doc.kitName;
+			var pets = doc.pets;
+			var earthquake = doc.earthquakeZone;
+			var flood = doc.floodZone;
+			var tornado = doc.tornadoZone;
+			var hurricane = doc.hurricaneZone;
+			var limited = doc.limitedStorage;
+			var backcountry = doc.backCountry;
+
+
 			UserModel.findOne(query, function(err,user){
 				ItemModel.find({}, function(err, items){
 					var allItems = {kitName:newKitName, timeCreated: new Date(),kitItems:[]};
+					//Pushes all general items (category 0)
 					for (var i = 0; i < items.length; i++) {
-						if (items[i].category.indexOf(1) > -1){
+						if (items[i].category.indexOf(0) > -1){
 							allItems.kitItems.push({itemName:items[i].itemName, description:items[i].description, quantity:items[i].quantity,unitOfMeasure:items[i].unitOfMeasure,category:items[i].category,expiration:items[i].expiration});
 						}
 					}
-					user.kits.push(allItems);
+					//Pushes all pet items (category 6)
+					if(pets > 0){
+						for (var i = 0; i < items.length; i++) {
+							if (items[i].category.indexOf(6) > -1){
+								allItems.kitItems.push({itemName:items[i].itemName, description:items[i].description, quantity:items[i].quantity,unitOfMeasure:items[i].unitOfMeasure,category:items[i].category,expiration:items[i].expiration});
+							}
+						}
+					}
+					//Pushes all earthquake items (category 1)
+					if(earthquake===true){
+						for (var i = 0; i < items.length; i++) {
+							if (items[i].category.indexOf(1) > -1){
+								allItems.kitItems.push({itemName:items[i].itemName, description:items[i].description, quantity:items[i].quantity,unitOfMeasure:items[i].unitOfMeasure,category:items[i].category,expiration:items[i].expiration});
+							}
+						}
+					}
+					//Pushes all flood items (category 2)
+					if(flood===true){
+						for (var i = 0; i < items.length; i++) {
+							if (items[i].category.indexOf(2) > -1){
+								allItems.kitItems.push({itemName:items[i].itemName, description:items[i].description, quantity:items[i].quantity,unitOfMeasure:items[i].unitOfMeasure,category:items[i].category,expiration:items[i].expiration});
+							}
+						}
+					}
+					//Pushes all tornado items (category 3)
+					if(tornado===true){
+						for (var i = 0; i < items.length; i++) {
+							if (items[i].category.indexOf(3) > -1){
+								allItems.kitItems.push({itemName:items[i].itemName, description:items[i].description, quantity:items[i].quantity,unitOfMeasure:items[i].unitOfMeasure,category:items[i].category,expiration:items[i].expiration});
+							}
+						}
+					}
+					//Pushes all limited items (category 4)
+					if(limited===true){
+						for (var i = 0; i < items.length; i++) {
+							if (items[i].category.indexOf(4) > -1){
+								allItems.kitItems.push({itemName:items[i].itemName, description:items[i].description, quantity:items[i].quantity,unitOfMeasure:items[i].unitOfMeasure,category:items[i].category,expiration:items[i].expiration});
+							}
+						}
+					}
+					//Pushes all backcountry items (category 5)
+					if(backcountry===true){
+						for (var i = 0; i < items.length; i++) {
+							if (items[i].category.indexOf(5) > -1){
+								allItems.kitItems.push({itemName:items[i].itemName, description:items[i].description, quantity:items[i].quantity,unitOfMeasure:items[i].unitOfMeasure,category:items[i].category,expiration:items[i].expiration});
+							}
+						}
+					}
+					// for (var i = 0; i < allItems.kitItems.length; i++) {
+					// 	if (allItems.kitItems[i].itemName.indexOf(allItems.kitItems[i].itemName) > -1){
+					// 		allItems.kitItems.splice(i);
+					// 	}
+					// }
+
+					console.log(uniqueArray);
+					// user.kits.push(allItems);
 					user.save(function(err, newUser){
 						res.render('userpage', {
 							profile: req.user,
